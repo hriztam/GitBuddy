d290fedad9aee6876e882ffa61bc77f24f943ed7
hriztam <hritamstark05@gmail.com>
2024-01-19 23:44:48 +0530

    Added light and dark theme functionality

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,3 +1,26 @@
-
-
 'use strict';
+
+const $HTML = document.documentElement;
+
+const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (sessionStorage.getItem("theme")) {
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+}
+
+let isPressed = false;
+
+const changeTheme = () => {
+    isPressed = isPressed ? false : true
+    $HTML.setAttribute("aria-pressed", isPressed);
+    $HTML.setAttribute("data-theme", ($HTML.dataset.theme === "light") ? "dark" : "light")
+    sessionStorage.setItem("theme", $HTML.dataset.theme)
+}
+
+window.addEventListener("load", () => {
+    const $themeBtn = document.querySelector("[data-theme-btn]")
+
+    $themeBtn.addEventListener("click", changeTheme)
+})
