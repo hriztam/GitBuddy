45cdde9924a8e1ff003af7ed3a4902b2a274d1c6
hriztam <hritamstark05@gmail.com>
2024-01-20 03:13:51 +0530

    added scroll and search toggle function in header

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,30 @@
-
 'use strict';
+
+/**
+ * @param {NodeList} $elements Nodelist
+ * @param {String} eventType String
+ * @param {Function} callback Function
+ */
+
+
+// Header scroll state
+window.addEventListener("DOMContentLoaded", () => {
+    const $header = document.querySelector("[data-header]")
+    const $searchToggler = document.querySelector("[data-search-toggler]")
+    const $searchField = document.querySelector("[data-search-field]")
+    let isExpanded = false;
+
+    window.addEventListener("scroll", () => {
+        $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+    })
+
+    $searchToggler.addEventListener("click", () => {
+        $header.classList.toggle("search-active");
+        isExpanded = isExpanded ? false : true;
+        $searchToggler.setAttribute("aria-expanded", isExpanded);
+        $searchField.focus();
+    })
+})
+
+
+
